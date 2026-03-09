53648f2f3295c36934f22e0ea8b1d8f3c2d1bb23
vinirod <vinicioswork@gmail.com>
2026-03-09 14:30:52 -0300

    feat: add internationalization with English and Portuguese (BR) support

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:perfect_numbers/core/di/injection.dart';
+import 'package:perfect_numbers/core/l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,6 +16,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Perfect Numbers',
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('en'), Locale('pt')],
       home: Scaffold(body: Center(child: Text('Perfect Numbers'))),
     );
   }
